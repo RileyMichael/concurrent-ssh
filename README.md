c14b123892b83d8241bea15ca6b90413a3b95305
RileyMichael <riley.mcgee.13@gmail.com>
2020-10-01 10:46:50 -0500

    add in input flags / update readme with example usage

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # concurrent-ssh
 easily execute ssh commands against multiple hosts
+
+### usage
+you can pass target hosts as comma separated inputs with `--targets` or `-t` shorthand:
+```bash
+cssh --targets host1,host2
+cssh -t host1,host2
+```
+or as a newline separated file with `--file` or `-f` shorthand:
+```bash
+cssh --file /some/file/path
+cssh -f /some/file/path
+```
+
+you can also pass args to ssh itself like so:
+```bash
+cssh --targets host1,host2 -- -o StrictHostKeyChecking=no whoami
+```
+
+You can override default concurrency limit of 25 by passing in `--limit` or `-l` shorthand
+```bash
+cssh --targets host1,host2 --limit 1 -- date
+```
+
+### todo
+- refactor to enable other tools (scp, rsync, etc)
+- expansion of targets (something like `--targets 127.0.0.{1..3}`)
+- better output (prepend hosts for clarity / blocks / colors)
+- tests (lol)
+- benchmark against pssh (anecdotally this is marginally faster on my machine with 1-10 hosts)
+- use goreleaser to build/publish releases
--- cmd/root.go
+++ cmd/root.go
@@ -1,20 +1,46 @@
 package cmd
 
 import (
+	"errors"
 	"fmt"
 	"github.com/spf13/cobra"
+	"io/ioutil"
 	"os"
 	"os/exec"
+	"strings"
 	"sync"
-	"time"
 )
 
 var command = &cobra.Command{
 	Use:   "cssh",
 	Short: "ssh, but concurrent",
 	Long:  `A simple tool to execute ssh commands against multiple hosts`,
-	Run: func(cmd *cobra.Command, args []string) {
-		ssh([]string{"host1", "host2"})
+	RunE: func(cmd *cobra.Command, args []string) error {
+		file, _ := cmd.Flags().GetString("file")
+		targets, _ := cmd.Flags().GetStringSlice("targets")
+		limit, _ := cmd.Flags().GetInt("limit")
+
+		if file != "" && len(targets) > 0 {
+			fmt.Println("Both file and targets input supplied; provide only 1.")
+			os.Exit(1)
+		} else if file == "" && len(targets) == 0 {
+			fmt.Println("Neither file or targets input supplied; provide 1.")
+			os.Exit(1)
+		}
+
+		if file != "" {
+			data, err := ioutil.ReadFile(file)
+
+			if err != nil {
+				return errors.New("error reading hosts file")
+			}
+
+			// trim last newline if exists & split on newlines
+			targets = strings.Split(strings.TrimSuffix(string(data), "\n"), "\n")
+		}
+
+		ssh(targets, args, limit)
+		return nil
 	},
 }
 
@@ -25,10 +51,13 @@ func Execute() {
 	}
 }
 
-func ssh(hosts []string) {
-	// todo: allow this as input with reasonable default
-	limit := 1
+func init() {
+	command.Flags().StringP("file", "f", "", "file containing target hosts")
+	command.Flags().StringSliceP("targets", "t", nil, "comma separated target hosts")
+	command.Flags().IntP("limit", "l", 25, "concurrency limit (default 25)")
+}
 
+func ssh(hosts []string, args []string, limit int) {
 	// buffered channel used to limit concurrency
 	limitChannel := make(chan struct{}, limit)
 
@@ -44,18 +73,15 @@ func ssh(hosts []string) {
 
 			// execute command
 			host := hosts[i]
-			fmt.Println("executing on host:", host)
 
-			// todo: args
-			cmd := exec.Command("ssh", host)
+			commandArgs := append([]string{host}, args...)
+			cmd := exec.Command("ssh", commandArgs...)
 			cmd.Stdout = os.Stdout
 			cmd.Stderr = os.Stderr
 
 			if err := cmd.Run(); err != nil {
 				fmt.Println("Error:", host, err)
 			}
-
-			time.Sleep(time.Second * 1)
 
 			// receive from channel to free up a spot
 			<-limitChannel
